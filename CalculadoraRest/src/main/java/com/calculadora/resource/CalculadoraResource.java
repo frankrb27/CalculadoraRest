e234d05a6e9b345481cf8d7e33812337a0b75f98
Frank Jaider Rodriguez Barreto <fraqjaroba9206@gmail.com>
2017-11-18 00:57:36 -0500

    Ajuste

--- CalculadoraRest/src/main/java/com/calculadora/resource/CalculadoraResource.java
+++ CalculadoraRest/src/main/java/com/calculadora/resource/CalculadoraResource.java
@@ -1,8 +1,7 @@
 package com.calculadora.resource;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("calculadora")
 public class CalculadoraResource {
 
-	@RequestMapping(path = "sumar", method = RequestMethod.GET,
+	@RequestMapping(path = "sumar/{num1}/{num2}", method = RequestMethod.GET,
 			consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
 			produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-	public Double suma(@PathParam("num1") Double num1, @PathParam("num2") Double num2) {
+	public Double suma(@PathVariable("num1") Double num1, @PathVariable("num2") Double num2) {
 		return num1 + num2;
 	}
 }
